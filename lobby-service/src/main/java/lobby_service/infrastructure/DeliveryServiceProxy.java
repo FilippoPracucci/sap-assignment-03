0ed4c332adda428ce53fe3fa39ef0be7aaf1338a
Filippo Pracucci <filippo.pracucci@studio.unibo.it>
2026-03-30 16:41:51 +0200

    feat(lobby-service): implement track delivery rejected

--- lobby-service/src/main/java/lobby_service/infrastructure/DeliveryServiceProxy.java
+++ lobby-service/src/main/java/lobby_service/infrastructure/DeliveryServiceProxy.java
@@ -35,11 +35,8 @@ public class DeliveryServiceProxy implements DeliveryService {
     static final String DELIVERY_TRACKING_REQUESTS_REJECTED_EVC = "delivery-{id}-tracking-requests-rejected";
 
     private final OutputEventChannel createDeliveryRequests;
-    private final InputEventChannel createDeliveryRequestsApproved;
-    private final InputEventChannel createDeliveryRequestsRejected;
 
-    private final Map<DeliveryId, OutputEventChannel> deliveryTrackingRequests = new HashMap<>();
-    private final Map<DeliveryId, InputEventChannel> deliveryTrackingRequestsRejected = new HashMap<>();
+    private final Map<String, OutputEventChannel> deliveryTrackingRequests = new HashMap<>();
 
     private final Map<String, Promise<DeliveryId>> deliveryIds = new HashMap<>();
     private final Map<String, Promise<String>> trackingSessionIds = new HashMap<>();
@@ -48,14 +45,14 @@ public class DeliveryServiceProxy implements DeliveryService {
     private int count = 0;
 
     public DeliveryServiceProxy(final Vertx vertx, final String evChannelsLocation)  {
+        final InputEventChannel createDeliveryRequestsApproved = new InputEventChannel(vertx,
+                CREATE_DELIVERY_REQUESTS_APPROVED_EVC, evChannelsLocation);
+        final InputEventChannel createDeliveryRequestsRejected = new InputEventChannel(vertx,
+                CREATE_DELIVERY_REQUESTS_REJECTED_EVC, evChannelsLocation);
         this.vertx = vertx;
         this.createDeliveryRequests = new OutputEventChannel(vertx, CREATE_DELIVERY_REQUESTS_EVC, evChannelsLocation);
-        this.createDeliveryRequestsApproved = new InputEventChannel(vertx, CREATE_DELIVERY_REQUESTS_APPROVED_EVC,
-                evChannelsLocation);
-        this.createDeliveryRequestsRejected = new InputEventChannel(vertx, CREATE_DELIVERY_REQUESTS_REJECTED_EVC,
-                evChannelsLocation);
-        this.createDeliveryRequestsApproved.init(e -> onCreateDeliveryRequestApproved(e, evChannelsLocation));
-        this.createDeliveryRequestsRejected.init(this::onCreateDeliveryRequestRejected);
+        createDeliveryRequestsApproved.init(e -> onCreateDeliveryRequestApproved(e, evChannelsLocation));
+        createDeliveryRequestsRejected.init(this::onCreateDeliveryRequestRejected);
     }
 
     private void onCreateDeliveryRequestApproved(final JsonObject event, final String evChannelsLocation) {
@@ -65,17 +62,14 @@ public class DeliveryServiceProxy implements DeliveryService {
         final String requestId = event.getString("requestId");
         this.deliveryIds.get(requestId).complete(new DeliveryId(deliveryId));
         this.deliveryTrackingRequests.put(
-                new DeliveryId(deliveryId),
+                deliveryId,
                 new OutputEventChannel(vertx, DELIVERY_TRACKING_REQUESTS_EVC.replace("{id}", deliveryId),
                         evChannelsLocation)
         );
         new InputEventChannel(vertx, DELIVERY_TRACKING_REQUESTS_APPROVED_EVC.replace("{id}", deliveryId),
                 evChannelsLocation).init(this::onDeliveryTrackingRequestsApproved);
-        this.deliveryTrackingRequestsRejected.put(
-                new DeliveryId(deliveryId),
-                new InputEventChannel(vertx, DELIVERY_TRACKING_REQUESTS_REJECTED_EVC.replace("{id}", deliveryId),
-                        evChannelsLocation)
-        );
+        new InputEventChannel(vertx, DELIVERY_TRACKING_REQUESTS_REJECTED_EVC.replace("{id}", deliveryId),
+                evChannelsLocation).init(this::onDeliveryTrackingRequestsRejected);
     }
 
     private void onCreateDeliveryRequestRejected(final JsonObject event) {
@@ -94,6 +88,14 @@ public class DeliveryServiceProxy implements DeliveryService {
         logger.info("Received: " + trackingSessionId);
         final String requestId = event.getString("requestId");
         this.trackingSessionIds.get(requestId).complete(trackingSessionId);
+    }
+
+    private void onDeliveryTrackingRequestsRejected(final JsonObject event) {
+        logger.info("onDeliveryTrackingRequestsRejected");
+        final String requestId = event.getString("requestId");
+        final String error = event.getString("error");
+        logger.severe(error);
+        this.trackingSessionIds.get(requestId).fail(new TrackDeliveryFailedException(error));
     }
 
     @Override
@@ -136,47 +138,27 @@ public class DeliveryServiceProxy implements DeliveryService {
         final JsonObject trackDeliveryEvent = new JsonObject();
         trackDeliveryEvent.put("deliveryId", deliveryId.id());
         trackDeliveryEvent.put("requestId", requestId);
-        this.deliveryTrackingRequests.get(deliveryId).postEvent(trackDeliveryEvent);
-
-        final Promise<String> promise = Promise.promise();
-        this.trackingSessionIds.put(requestId, promise);
         try {
-            return promise.future().toCompletionStage()
+            final Promise<String> promise = Promise.promise();
+            this.deliveryTrackingRequests.get(deliveryId.id()).postEvent(trackDeliveryEvent);
+            this.trackingSessionIds.put(requestId, promise);
+            return promise.future()
+                    .toCompletionStage()
                     .toCompletableFuture()
                     .get(10, TimeUnit.SECONDS);
+        } catch (final ExecutionException e) {
+            this.trackingSessionIds.remove(requestId);
+            final Throwable cause = e.getCause();
+            logger.info("TrackDeliveryFailedException with cause: " + cause.getMessage());
+            if (cause instanceof TrackDeliveryFailedException) {
+                throw (TrackDeliveryFailedException) cause;
+            }
+            throw new TrackDeliveryFailedException();
+        } catch (final NullPointerException e) {
+            throw new TrackDeliveryFailedException("Delivery not found");
         } catch (final Exception e) {
             this.trackingSessionIds.remove(requestId);
             throw new ServiceNotAvailableException();
         }
-
-        /*final HttpClient client = HttpClient.newHttpClient();
-        final JsonObject body = new JsonObject();
-        body.put("deliveryId", deliveryId.id());
-
-        final String trackDeliveryResourceEndpoint = serviceURI + "/api/v1/deliveries/" + deliveryId.id() + "/track";
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(trackDeliveryResourceEndpoint))
-                .header("Accept", "application/json")
-                .POST(HttpRequest.BodyPublishers.ofString(body.toString()))
-                .build();
-
-        HttpResponse<String> response;
-        try {
-            response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            System.out.println("Response Code: " + response.statusCode());
-        } catch (Exception ex) {
-            logger.severe(ex.getMessage());
-            throw new TrackDeliveryFailedException();
-        }
-
-        if (response.statusCode() != 200) {
-            System.out.println("POST request failed: " + response.body());
-            throw new ServiceNotAvailableException();
-        }
-        final JsonObject responseBody = new JsonObject(response.body());
-        if (responseBody.getString("result").equals("error")) {
-            throw new TrackDeliveryFailedException(responseBody.getString("error"));
-        }
-        return responseBody.getString("trackingSessionId");*/
     }
 }
